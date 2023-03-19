78b2d3e75cc1b79d9a8628e59bcd2470fdd484d8
Gameciel <christofer.calvin.pranata@gmail.com>
2023-03-19 23:49:08 +0700

    Revert "MySQL Integration"
    This reverts commit dbb00794a9b8dce178282bc9053d626d345a4801.

--- controller/expenseController.js
+++ controller/expenseController.js
@@ -1,51 +1,55 @@
-import {
-	getExpensesByQuery,
-	getExpenseByID,
-	removeExpense,
-	insertNewExpense,
-	modifyExpense,
-} from "../logic/expenseServicesMySQL.js";
+import { 
+	getExpensesByQuery, 
+	getExpenseByID, 
+	deleteExpenseByID, 
+	insertNewExpense, 
+	modifyExpense 
+} from "../logic/expenseServices.js";
 
-export const getExpenses = async (request, response) => {
-	const filteredResult = await getExpensesByQuery(request.query);
 
-	filteredResult.length
+export const getExpenses = (request, response) => {
+	const filteredResult = getExpensesByQuery(request.query);
+
+	filteredResult.length 
 		? response.status(200).send(filteredResult)
 		: response.status(404).send(`Not Found.`);
 };
 
-export const getExpense = async (request, response) => {
-	const resultByID = await getExpenseByID(request.params.id);
+
+export const getExpense = (request, response) => {
+	const resultByID = getExpenseByID(request.params.id);
+
+
 
 	resultByID.length
 		? response.status(200).send(resultByID)
 		: response.status(404).send(`Not Found.`);
 };
 
+
 export const postExpense = (request, response) => {
 	const postStatus = insertNewExpense(request.body);
-
+	
 	postStatus
 		? response.status(200).send("Data has been added.")
 		: response.status(400).send("Bad Request, have you put body on request?");
 };
 
+
 export const deleteExpense = (request, response) => {
-	const deleteStatus = removeExpense(request.params.id);
+	const deleteStatus = deleteExpenseByID(request.params.id);
 
 	deleteStatus
-		? response
-				.status(200)
-				.send(`Data for id ${request.params.id} has been deleted.`)
+		? response.status(200).send(`Data for id ${request.params.id} has been deleted.`)
 		: response.status(404).send(`Not Found.`);
 };
+
 
 export const patchExpense = (request, response) => {
 	const patchStatus = modifyExpense(request.body, request.params.id);
 
 	patchStatus
-		? response
-				.status(200)
-				.send(`Data for id ${request.params.id} has been patched.`)
+		? response.status(200).send(`Data for id ${request.params.id} has been patched.`)
 		: response.status(400).send(`Bad Request, no body / ID not found.`);
 };
+
